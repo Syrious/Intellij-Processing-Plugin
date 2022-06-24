9c00b935a1fd7aa1adc259432b7de0ecff571800
GitHub Action <action@github.com>
2022-06-24 09:56:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/syrious/processingintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.syrious.processingintellijplugin.services
+
+import com.github.syrious.processingintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/syrious/processingintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/syrious/processingintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.syrious.processingintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.syrious.processingintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
